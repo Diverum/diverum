18f2c25f28d0e6ee50db2334d75ab35a71217660
jhoxar <jhon98-ra@hotmail.com>
2026-02-12 21:04:41 -0500

    merging

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,6 +3,9 @@ export function Counter (){
     return(
         <>
             <h2>This page is working</h2>
+            <div>
+                <p>hello world</p>
+            </div>
             
         </>
        
