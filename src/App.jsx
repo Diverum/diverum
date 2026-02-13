7c00c1e511a503cc97d54f1334afe0b5d7f44375
jhoxar <jhon98-ra@hotmail.com>
2026-02-12 22:33:05 -0500

    testing tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ function App() {
 
   return (
     <>
-      <h1>Hello sir</h1>
-      
+      <h1 className="text-3xl font-bold underline">Diverum</h1>
+      <button className="bg-blue-500 text-white p-2 rounded-md">Click me</button>
+      <input type="text" className="border-2 border-gray-300 rounded-md p-2" />
+      <select className="border-2 border-gray-300 rounded-md p-2">
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+      <textarea className="border-2 border-gray-300 rounded-md p-2"></textarea>
     </>
   )
 }
